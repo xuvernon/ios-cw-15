a665a669e65844e2eee46f01e0d3cf9eec077ab5
sarah almutari <sara0moha78@gmail.com>
2022-03-19 10:48:55 +0300

    done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+struct Student {
+var name: String
+var age: Int
+var grade: Double
+var attendence: Bool
+}
+
+var sarah = Student(name: "sarah", age: 16, grade: 90, attendence: true)
+print(sarah)
